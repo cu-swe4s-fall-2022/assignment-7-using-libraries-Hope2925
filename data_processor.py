7601044f0b346c8a787935e8b461c3a8cd08df63
Hope2925 <hopekirby2@gmail.com>
2022-10-22 16:05:46 -0600

    made function that passed initial test, added another test and it still passed

--- data_processor.py
+++ data_processor.py
@@ -1,7 +1,25 @@
 # remember to import your libraries!
+import numpy as np
+import random
+
 
 def get_random_matrix(num_rows, num_columns):
-	return None
+	"""
+	This function will create an return an N(rows) by M(col)
+	matrix of random floating point numbers sampled from a uniform
+	distribution with a range of (0,1] (inclusive of 1)
+	Parameters:
+		- num_rows: integer desired # rows
+		- num_columns: integer desired # columns
+	Returns:
+		- 2D numpy array [rows][columns]
+	"""
+	# since normally [low, high) need to do 0.00000001 & 1.00000001 for range
+	low, high = 0.00000001, 1.00000001
+	uniform_matrix = np.random.uniform(low=low,
+									high=high,
+									size=(num_rows, num_columns))
+	return uniform_matrix
 
 def get_file_dimensions(file_name):
 	return (0,0)
--- tests/test_data_processor.py
+++ tests/test_data_processor.py
@@ -8,10 +8,16 @@ import random
 class MyTestCase(unittest.TestCase):
     def test_get_random_matrix(self):
         # ensure the correct # rows & # columns are used
-        num_rows = random.randint(0, 30)
-        num_cols = random.randint(0, 30)
+        num_rows = random.randint(1, 30)
+        num_cols = random.randint(1, 30)
         self.assertEqual(dp.get_random_matrix(num_rows, num_cols).shape,
                          (num_rows, num_cols))
+        # ensure all values are above 0 and can include 1 but not higher
+        for rownum in range(num_rows):
+            for colnum in range(num_cols):
+                self.assertTrue(0 <
+                    dp.get_random_matrix(num_rows, num_cols)[rownum][colnum]
+                    <= 1)
 
 
 if __name__ == '__main__':
