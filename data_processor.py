7b2748e4a04545399537c8d5be913bbc81545b0a
Hope2925 <hopekirby2@gmail.com>
2022-10-23 19:56:27 -0600

    made pycodestyle friendly

--- data_processor.py
+++ data_processor.py
@@ -7,82 +7,84 @@ sys.path.append("./tests/")
 
 
 def get_random_matrix(num_rows, num_columns):
-	"""
-	This function will create an return an N(rows) by M(col)
-	matrix of random floating point numbers sampled from a uniform
-	distribution with a range of (0,1] (inclusive of 1)
-	Parameters:
-		- num_rows: integer desired # rows, must be > 0
-		- num_columns: integer desired # columns, must be > 0
-	Returns:
-		- 2D numpy array [rows][columns]
-	"""
-	# check that input is greater than 0
-	if (num_rows > 0 and num_columns > 0):
-		# since normally [low, high) need to do 0.00000001 & 1.00000001 for range
-		low, high = 0.00000001, 1.00000001
-		uniform_matrix = np.random.uniform(low=low,
-										high=high,
-										size=(num_rows, num_columns))
-	else:
-		raise ValueError("# of cols & rows must be an int > 0")
-	return uniform_matrix
+    """
+    This function will create an return an N(rows) by M(col)
+    matrix of random floating point numbers sampled from a uniform
+    distribution with a range of (0,1] (inclusive of 1)
+    Parameters:
+        - num_rows: integer desired # rows, must be > 0
+        - num_columns: integer desired # columns, must be > 0
+    Returns:
+        - 2D numpy array [rows][columns]
+    """
+    # check that input is greater than 0
+    if (num_rows > 0 and num_columns > 0):
+        # since normally [low, high) do 0.00000001 & 1.00000001 for range
+        low, high = 0.00000001, 1.00000001
+        uniform_matrix = np.random.uniform(low=low,
+                                           high=high,
+                                           size=(num_rows, num_columns))
+    else:
+        raise ValueError("# of cols & rows must be an int > 0")
+    return uniform_matrix
+
 
 def get_file_dimensions(file_name):
-	"""
-	This function takes in the name of a comma separated value (csv)
-	file, reads in the file, and returns the dimensions of the tabular data
-	(rows, columns).
-	Parameters:
-		file_name: string of a csv file to check dimensions of
-	Returns:
-		tuple of file dimensions (#rows, #cols)
-	"""
-	# open file
-	f = open(file_name, 'r')
-	# start count
-	num_rows = 0
-	fin_num_cols = 0
-	for line in f:
-		# get the # cols for first line (if empty would be 0)
-		num_cols = len(line.split(sep=","))
-		# if not an empty line
-		if (num_cols > 1):
-			# add 1 to count for each line
-			num_rows += 1
-			# get final # rows
-			fin_num_cols = num_cols
-		else:
-			print("row", num_rows, "is not comma separated")
-	# close file
-	f.close()
-	if fin_num_cols == 0:
-		raise Exception("Unless there is only 1 column, file is not comma separated")
-	return (num_rows, fin_num_cols)
+    """
+    This function takes in the name of a comma separated value (csv)
+    file, reads in the file, and returns the dimensions of the tabular data
+    (rows, columns).
+    Parameters:
+        file_name: string of a csv file to check dimensions of
+    Returns:
+        tuple of file dimensions (#rows, #cols)
+    """
+    # open file
+    f = open(file_name, 'r')
+    # start count
+    num_rows = 0
+    fin_num_cols = 0
+    for line in f:
+        # get the # cols for first line (if empty would be 0)
+        num_cols = len(line.split(sep=","))
+        # if not an empty line
+        if (num_cols > 1):
+            # add 1 to count for each line
+            num_rows += 1
+            # get final # rows
+            fin_num_cols = num_cols
+        else:
+            print("row", num_rows, "is not comma separated")
+    # close file
+    f.close()
+    if fin_num_cols == 0:
+        raise Exception("Unless there is 1 col, file is not comma separated")
+    return (num_rows, fin_num_cols)
+
 
 def write_matrix_to_file(num_rows, num_columns, file_name):
-	"""
-	This function takes the number of desired rows and columns, and
-	writes matrix of that size of random numbers from a uniform
-	distribution within range (0, 1] and then writes it to a
-	csv file.
-	Parameters
-		- num_rows: # of rows, integer > 0
-		- num_columns: # of columns, integer > 0
-		- file_name: name of a csv file to save the matrix in
-			- (must end in .csv)
-	Returns:
-		- outputs a file of the given file name with the
-			created matrix in csv form
-	"""
-	# make an exception if file name does not end with csv
-	file_name_split = file_name.split(sep=".")
-	if file_name_split[len(file_name_split)-1] != "csv":
-		# if not then add & raise exception to warn user
-		file_name = file_name + ".csv"
-		raise Exception("Original name did not end in csv so adding it")
-	# make appropriately sized matrix
-	matrix_to_write = get_random_matrix(num_rows, num_columns)
-	# write matrix to file as csv
-	np.savetxt(file_name, matrix_to_write, delimiter=',')
-	return None
+    """
+    This function takes the number of desired rows and columns, and
+    writes matrix of that size of random numbers from a uniform
+    distribution within range (0, 1] and then writes it to a
+    csv file.
+    Parameters
+        - num_rows: # of rows, integer > 0
+        - num_columns: # of columns, integer > 0
+        - file_name: name of a csv file to save the matrix in
+            - (must end in .csv)
+    Returns:
+        - outputs a file of the given file name with the
+            created matrix in csv form
+    """
+    # make an exception if file name does not end with csv
+    file_name_split = file_name.split(sep=".")
+    if file_name_split[len(file_name_split)-1] != "csv":
+        # if not then add & raise exception to warn user
+        file_name = file_name + ".csv"
+        raise Exception("Original name did not end in csv so adding it")
+    # make appropriately sized matrix
+    matrix_to_write = get_random_matrix(num_rows, num_columns)
+    # write matrix to file as csv
+    np.savetxt(file_name, matrix_to_write, delimiter=',')
+    return None
---
+++ tests/unit/test_data_processor.py
@@ -0,0 +1,74 @@
+import sys
+from os import path
+import unittest
+import data_processor as dp
+import random
+
+sys.path.append("../..")
+
+
+class TestUtils(unittest.TestCase):
+    # set up shared data to test with
+    @classmethod
+    def setUpClass(cls):
+        # random row & col numbers
+        cls.num_rows = random.randint(1, 30)
+        cls.num_cols = random.randint(1, 30)
+        # iris data with known dimensions
+        cls.iris_file_name = '../iris.data'
+        # fake tab delimited file
+        cls.tbd_file_name = "../extbdfile.txt"
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.num_rows = None
+        cls.num_cols = None
+        cls.iris_file_name = None
+        cls.tbd_file_name = None
+
+    def test_get_random_matrix(self):
+        # ensure the correct # rows & # columns are used
+        self.assertEqual(dp.get_random_matrix(self.num_rows,
+                                              self.num_cols).shape,
+                         (self.num_rows, self.num_cols))
+        # ensure all values are above 0 and can include 1 but not higher
+        for rownum in range(self.num_rows):
+            for colnum in range(self.num_cols):
+                self.assertTrue(0 <
+                                dp.get_random_matrix(self.num_rows,
+                                                     self.num_cols)[rownum][colnum]
+                                <= 1)
+        # ensure raises error if use 0 in either
+        self.assertRaises(ValueError, dp.get_random_matrix, 0, self.num_cols)
+        self.assertRaises(ValueError, dp.get_random_matrix, self.num_rows, 0)
+
+    def test_get_file_dimensions(self):
+        # use iris data which should have 150 rows & 5 columns
+        self.assertEqual(dp.get_file_dimensions(self.iris_file_name),
+                         (150, 5))
+        # if not csv then raises exception
+        self.assertRaises(Exception,
+                          dp.get_file_dimensions,
+                          self.tbd_file_name)
+
+    def test_write_matrix_to_file(self):
+        written_file_name = 'written_file.csv'
+        dp.write_matrix_to_file(self.num_rows,
+                                self.num_cols,
+                                written_file_name)
+        # confirm path created for file with appropriate name in same folder
+        self.assertEqual(path.exists(written_file_name), True)
+        # confirm file is actually a file
+        self.assertEqual(path.isfile(written_file_name), True)
+        # confirm file made contains appropriate # dimensions (& csv file)
+        self.assertEqual(dp.get_file_dimensions(written_file_name),
+                         (self.num_rows, self.num_cols))
+        # raises error if file name does not end with csv
+        self.assertRaises(Exception,
+                          dp.write_matrix_to_file,
+                          1, 2,
+                          "nocsv.txt")
+
+
+if __name__ == '__main__':
+    unittest.main()
